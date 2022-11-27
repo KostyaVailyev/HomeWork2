3ae381203dd387edf5c976e71418e0d684c8e266
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-11-27 20:45:24 +0300

    add thirdNumber

--- thirdNumber/thirdNumber.cs
+++ thirdNumber/thirdNumber.cs
@@ -4,4 +4,4 @@ int num = Convert.ToInt32(Console.ReadLine());
 int num1 = num % 10;
 int num2 = num / 10 % 10;
 int num3 = num / 100 % 10;
-Console.WriteLine(num2 == 0 ? $"В числе {num} нет второй цифры." : $"Вторая цифра числа {num} это {num2}");
+Console.WriteLine(num3 == 0 ? $"В числе {num} нет третей цифры." : $"Третья цифра числа {num} это {num1}");
