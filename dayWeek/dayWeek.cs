7f51284cb6b94ed0ff57745f3c6a9cff26e925f6
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-11-27 21:08:42 +0300

    add dayWeek

---
+++ dayWeek/dayWeek.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня недели:");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num <= 5)
+Console.WriteLine("Не выходной день.");
+else if (num == 6)
+Console.WriteLine("Выходной.");
+else if (num == 7)
+Console.WriteLine("Выходной.");
+else Console.WriteLine("Не является днем недели.");
